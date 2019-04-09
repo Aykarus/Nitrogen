5204d6db876e2409304a00827139140c2aa18e69
Chester Cari <chester_cari@outlook.com>
2019-04-08 20:27:37 -0400

    Fix Layout

--- script.js
+++ script.js
@@ -16,55 +16,53 @@
 // });
 
 function addGoals() {
-  //get info person wrote in text box
-  var goal = document.getElementById('prompt').value;
+    //get info person wrote in text box
+    var goal = document.getElementById('prompt').value;
 
-  //create an li item
-  var li = document.createElement("li");
-  li.innerHTML = item;
+    //create an li item
+    var li = document.createElement("li");
+    li.innerHTML = item;
 
-  //add to goals list
-  document.getElementById("goals").appendChild(li)
+    //add to goals list
+    document.getElementById("goals").appendChild(li)
+}
+        // //create a remove button
+        // var removeButton = document.createElement("button")
+        // removeButton.innerHTML = "Remove"
+        //
+        // //add button to the list
+        // document.getElementById("goals").appendChild(removeButton);
+        //
+        // //clear the text input inputBox
+        // document.getElementById("prompt").value = "";
 
-  // //create a remove button
-  // var removeButton = document.createElement("button")
-  // removeButton.innerHTML = "Remove"
-  //
-  // //add button to the list
-  // document.getElementById("goals").appendChild(removeButton);
-  //
-  // //clear the text input inputBox
-  // document.getElementById("prompt").value = "";
-=======
-var date = getTime();
+    function startTime() {
+        var today = new Date();
+        var h = today.getHours();
+        var m = today.getMinutes();
+        //var h = 8;
+        //var s = today.getSeconds();
+        m = checkTime(m);
+        //s = checkTime(s);
+        document.getElementById('time').innerHTML =
+            h + ":" + m;
+        var t = setTimeout(startTime, 500);
 
-function startTime() {
-    var today = new Date();
-    var h = today.getHours();
-    var m = today.getMinutes();
-    //var h = 8;
-    //var s = today.getSeconds();
-    m = checkTime(m);
-    //s = checkTime(s);
-    document.getElementById('time').innerHTML =
-        h + ":" + m;
-    var t = setTimeout(startTime, 500);
-
-    if (h < 11 && h > 5) {
-        document.getElementById("message").innerHTML =
-        "Good Morning ";
-    }else if (h > 11 && h < 17) {
-        document.getElementById("message").innerHTML =
-        "Good Afternoon ";
-    }else {
-        document.getElementsById('message').innerHTML =
-        "Good Evening ";
+        if (h < 11 && h > 5) {
+            document.getElementById("message").innerHTML =
+                "Good Morning ";
+        } else if (h > 11 && h < 17) {
+            document.getElementById("message").innerHTML =
+                "Good Afternoon ";
+        } else {
+            document.getElementsById('message').innerHTML =
+                "Good Evening ";
+        }
     }
-}
 
-function checkTime(i) {
-    if (i < 10) {
-        i = "0" + i
-    }; // add zero in front of numbers < 10
-    return i;
-}
+    function checkTime(i) {
+        if (i < 10) {
+            i = "0" + i
+        }; // add zero in front of numbers < 10
+        return i;
+    }
